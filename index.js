dcca65ae22579108ca6b5d30cf0f0edf541e1bfb
Jean <jean_pierre_97@outlook.com>
2023-07-28 10:36:11 -0300

    feat: added chat history

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ async function main() {
     console.log(colors.bold.green('Welcome to the chatBot ^-^'))
     console.log(colors.bold.green('You can now chat with the bot.'))
 
+    const chatHistory = []
+
     while (true) {
         const userInput = readlineSync.question(colors.yellow('You: '))
 
         try {
+            // construct messages by interating over history
+            const messages = chatHistory.map(([role, content]) => ({ role, content }))
+            // add latest userInput
+            messages.push({ role: 'user', content: userInput })
+
             const chatCompletion = await openai.createChatCompletion({
                 model: 'gpt-3.5-turbo',
-                messages: [
-                    { role: 'user', content: userInput }
-                ]
+                messages: messages
             })
             const response = chatCompletion.data.choices[0].message.content
 
@@ -23,6 +28,9 @@ async function main() {
             if (userInput.toLocaleLowerCase() === 'exit') {
                 return
             }
+            // update history with user input and assistant response
+            chatHistory.push(['user', userInput])
+            chatHistory.push(['assistant', response])
 
         } catch (error) {
             console.error(colors.red(error))
